3cd81b1af47e288ce11247ea45e88bc087908959
Prasoon <prasoonsoni.work@gmail.com>
2022-06-03 18:15:38 +0530

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<center><h1>Welcome to Gramoday Task</h1>' +
+        '<h3><a href="https://github.com/prasoonsoni/Gramoday-Task" target="_blank">Visit Repository</a></h3></center>')
+})
+
+app.listen(port, () => {
+    console.log(`Gramoday Task listening on http://localhost:${port}`)
+})
